363aff19437ab877348f0ef4980e47b83ba091cc
rafaalmeida1 <rafaempresarial312@gmail.com>
2022-10-20 14:23:01 -0300

    change url to api

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: "https://daily-planner-ochre.vercel.app/api"
+    baseURL: "https://daily-planner-pos.vercel.app/api"
 })
