c2d2d7301a4f0771824777cc6ab77f7fa4592c36
rafaalmeida1 <rafaempresarial312@gmail.com>
2022-10-29 16:07:11 -0300

    bug fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    // baseURL: "https://daily-planner-pos.vercel.app/api"
     baseURL: "https://daily-planner-pos.vercel.app/api"
+    // baseURL: "https://daily-planner-pos.vercel.app/api"
 })
